474c9f742da74935b3d699a768a506ce5a88b0df
vosmerkin <vosmerkin@mail.ru>
2021-03-27 12:01:38 +0200

    commit from home

--- src/Lucky.java
+++ src/Lucky.java
@@ -4,7 +4,7 @@ public class Lucky {
 
     public static final int numTries = 5;
 
-
+//user makes 5 tries
     static boolean[] userMakesTries(int tries) {
         Scanner in = new Scanner(System.in);
         boolean[] userGuess = new boolean[tries];
